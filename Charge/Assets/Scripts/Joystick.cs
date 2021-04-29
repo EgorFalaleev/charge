3c250c2658e55243c04a053baa27f90865adf68f
EgorFalaleev <Egor1268@gmail.com>
2021-04-29 12:16:53 +0300

    Added mouse joystick movement

---
+++ Charge/Assets/Scripts/Joystick.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float movementSpeed;
+
+    // state variables
+    private bool isTouchStarted;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // determine the center point of the joystick
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            isTouchStarted = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else isTouchStarted = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isTouchStarted)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1f);
+
+            Move(direction);
+        }
+    }
+
+    private void Move(Vector2 direction)
+    {
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+}
