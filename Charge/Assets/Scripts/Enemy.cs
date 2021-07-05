6af2e477bc4540d48523c5d184490c3f09d7e5b6
EgorFalaleev <Egor1268@gmail.com>
2021-07-05 17:19:48 +0300

    Added enemy health text

--- Charge/Assets/Scripts/Enemy.cs
+++ Charge/Assets/Scripts/Enemy.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     // state variables
     [SerializeField] private float health = 100;
+    [SerializeField] private Text healthText;
+
+    private void Start()
+    {
+        healthText.text = "Enemy Health " + health;
+    }
 
     public void GetDamage(float damagePoints)
     {
         health -= damagePoints;
+
+        healthText.text = "Enemy Health " + health;
 
         if (health <= 0) Destroy(gameObject);
     }
