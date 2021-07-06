39fa9c0250e3b53c63f12062ea8f83852432b5e0
EgorFalaleev <Egor1268@gmail.com>
2021-07-06 14:45:01 +0300

    Added framerate independance to camera following

--- Charge/Assets/Scripts/CameraFollow.cs
+++ Charge/Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // configuration parameters
-    [SerializeField] private float smoothness = 0.1f;
+    [SerializeField] private float smoothTime = 10f;
     [SerializeField] private Vector3 cameraOffset;
 
     // references
@@ -15,6 +15,6 @@ public class CameraFollow : MonoBehaviour
       private void LateUpdate()
       {
           Vector3 desiredPosition = targetTf.position + cameraOffset;
-          transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothness);
+          transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime * Time.deltaTime);
       }
 }
